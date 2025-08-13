c3b7756c4a48175a3f21d849b89845bed14cf762
Andres Anzola <af.anzola@outlook.com>
2025-08-13 13:14:48 -0500

    Add a window with a button using a global scope

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # Importing the components we need
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # The sys module is responsible for processing command line arguments.
 import sys
-
 app = QApplication(sys.argv)
 
-window = QWidget()
-window.show()
+window = QMainWindow()
+window.setWindowTitle("Our first MainWindow App!")
 
-# Start the event loop
+button = QPushButton()
+button.setText("Press me")
+
+window.setCentralWidget(button)
+
+window.show()
 app.exec()
