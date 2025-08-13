637c3d4c625824e76e244382129e0ad64fe14021
Andres Anzola <af.anzola@outlook.com>
2025-08-13 13:23:27 -0500

    Changed code to apply object based scope

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # The sys module is responsible for processing command line arguments.
 import sys
+
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button Holder App")
+        button = QPushButton("Press Me!")
+
+        # Set up the button as our central widget
+        self.setCentralWidget(button)
+
 app = QApplication(sys.argv)
 
-window = QMainWindow()
-window.setWindowTitle("Our first MainWindow App!")
-
-button = QPushButton()
-button.setText("Press me")
-
-window.setCentralWidget(button)
-
+window = ButtonHolder()
 window.show()
 app.exec()
