ceca3dfd7d983a7bddcc0a09b861ae129d5d2656
Andres Anzola <af.anzola@outlook.com>
2025-08-13 12:57:08 -0500

    Add comments to clarify imports and commands

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# Importing the components we need
 from PySide6.QtWidgets import QApplication, QWidget
+
+# The sys module is responsible for processing command line arguments.
 import sys
 
 app = QApplication(sys.argv)
@@ -6,4 +9,5 @@ app = QApplication(sys.argv)
 window = QWidget()
 window.show()
 
+# Start the event loop
 app.exec()
