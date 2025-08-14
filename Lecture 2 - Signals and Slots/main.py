21bace876c4dc5e66a733a385370f9ed10cfae0a
Andres Anzola <af.anzola@outlook.com>
2025-08-13 21:14:55 -0500

    Modified a button property and added a state checker in the slot

--- Lecture 2 - Signals and Slots/main.py
+++ Lecture 2 - Signals and Slots/main.py
@@ -1,11 +1,14 @@
 from PySide6.QtWidgets import QApplication, QPushButton
 
 # The slot: a method which recieves a signal and does something
-def button_clicked():
-    print("You clicked the button, didn't you!")
+def button_clicked(state):
+    print("You clicked the button, didn't you!", state)
 
 app = QApplication()
 button = QPushButton("Press Me")
+
+# The following method can make the button checkable
+button.setCheckable(True)
 
 # The signal: emitted from the widget to a slot, in order to
 # make an action happen
