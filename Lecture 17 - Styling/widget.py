58432b67e82b81cfd1a729a20149baadc672f314
Andres Anzola <af.anzola@outlook.com>
2025-08-31 19:48:34 -0500

    Created live style sheet customization with various testing widgets

--- Lecture 17 - Styling/widget.py
+++ Lecture 17 - Styling/widget.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import (QWidget, QApplication, QGridLayout, QGroupBox,
                                QComboBox, QLabel, QLineEdit, QPushButton,
-                               QCheckBox, QTextEdit, QVBoxLayout, QStyleFactory)
+                               QCheckBox, QPlainTextEdit, QVBoxLayout, 
+                               QStyleFactory, QSpinBox)
 from PySide6.QtGui import QPalette, QColorConstants, QColor
 
 # This lecture will show how styling works in both the whole application and to
@@ -66,8 +67,50 @@ class MainWidget(QWidget):
         self.group2_textCombo.currentTextChanged.connect(self.change_text_color)
         self.group2_backgroundCombo.currentTextChanged.connect(self.change_background_color)
 
+        # Demo 3: StyleSheets
+
+        # While QPalette is a great tool for configuring color, it isn't able
+        # to change other visual properties of a widget, for example text size,
+        # width, height, border type, etc. This is where StyleSheets come in.
+        # In essence, they are instructions on how a specific widget will look
+        # like. For this, it uses a similar structure to CSS to set every
+        # parameter needed, like this
+
+        """
+        Widget {
+            parameter1 : value;
+            parameter2 : value;
+            ...
+        }
+        """
+
+        # Visual properties mostly follow the CSS standard properties, but the
+        # official supported ones are listed in the PySide6 documentation:
+        # https://doc.qt.io/qtforpython-6/overviews/qtgui-richtext-html-subset.html#css-properties
+
+        self.group3 = QGroupBox('Styling Sheets')
+        group3_layout = QVBoxLayout()
+        self.group3_textEdit = QPlainTextEdit()
+        self.group3_checkbox = QCheckBox('Checkbox')
+        self.group3_comboBox = QComboBox(editable=False, placeholderText='Selectable')
+        self.group3_spinBox = QSpinBox(minimum=0, maximum=100, value=0)
+        self.group3_label = QLabel('Text')
+        self.group3_lineEdit = QLineEdit(placeholderText='This is a text')
+        self.group3_button = QPushButton('Button')
+        group3_layout.addWidget(self.group3_textEdit)
+        group3_layout.addWidget(self.group3_checkbox)
+        group3_layout.addWidget(self.group3_comboBox)
+        group3_layout.addWidget(self.group3_spinBox)
+        group3_layout.addWidget(self.group3_label)
+        group3_layout.addWidget(self.group3_lineEdit)
+        group3_layout.addWidget(self.group3_button)
+        self.group3.setLayout(group3_layout)
+
+        self.group3_textEdit.textChanged.connect(self.set_style_sheet)
+
         mainLayout.addWidget(group1, 0,0)
         mainLayout.addWidget(group2, 1,0)
+        mainLayout.addWidget(self.group3, 0,1, 2,1)
 
         self.setLayout(mainLayout)
 
@@ -81,5 +124,11 @@ class MainWidget(QWidget):
     def change_background_color(self, color):
         self.globalPalette.setColor(QPalette.ColorRole.Window, QColor.fromString(color))
         self.group2_testLabel.setPalette(self.globalPalette)
+
+    def set_style_sheet(self):
+        qss = self.group3_textEdit.toPlainText()
+        # The Qt engine will automatically parse the style sheet, and apply
+        # the changes if it's valid.
+        self.group3.setStyleSheet(qss)
 
         
