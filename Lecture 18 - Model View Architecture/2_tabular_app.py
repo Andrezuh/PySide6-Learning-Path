1590f0e1a7c2386cd470a6e3a81368682b22be34
Andres Anzola <af.anzola@outlook.com>
2025-09-07 00:02:54 -0500

    Demonstrate the decoration role with icons and different value types

--- Lecture 18 - Model View Architecture/2_tabular_app.py
+++ Lecture 18 - Model View Architecture/2_tabular_app.py
@@ -1,20 +1,24 @@
 from PySide6.QtWidgets import (QWidget, QMainWindow, QApplication, QTableView)
 from PySide6.QtCore import QPersistentModelIndex, Qt, QAbstractTableModel, QModelIndex
-from PySide6.QtGui import QColor
+from PySide6.QtGui import QColor, QIcon
 import sys
 from datetime import datetime
+from os import path
 
 hue = ['#053061', '#2166ac', '#4393c3', '#92c5de', '#d1e5f0',
 '#f7f7f7', '#fddbc7', '#f4a582', '#d6604d', '#b2182b', '#67001f']
+
+basedir = path.dirname(__file__)
 
 class TableModel(QAbstractTableModel):
     def __init__(self, data):
         super().__init__()
         self._data = data
     
-    # DEMO 1: Value Formatting
+    """
     def data(self, index:QModelIndex, role):
-        
+
+        # DEMO 1: Value Formatting
         if role == Qt.ItemDataRole.DisplayRole:
             value =  self._data[index.row()][index.column()]
 
@@ -37,20 +41,20 @@ class TableModel(QAbstractTableModel):
         # cell, like alignment, text color, background color and/or hue, etc.
 
         # DEMO 2: Text color
-        """ 
+
         if role == Qt.ItemDataRole.BackgroundRole and index.column() == 2:
             return QColor(Qt.GlobalColor.blue)
-        """
+
         # DEMO 3: Text alignment
-        """ 
+
         if role == Qt.ItemDataRole.TextAlignmentRole:
             value = self._data[index.row()][index.column()]
 
             if isinstance(value, int) or isinstance(value, float):
                 return Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignRight
-        """
+
         # DEMO 4: Text colors
-        """ 
+
         if role == Qt.ItemDataRole.ForegroundRole:
             value = self._data[index.row()][index.column()]
 
@@ -58,7 +62,7 @@ class TableModel(QAbstractTableModel):
                 isinstance(value, int) or isinstance(value, float)
             ) and value < 0:
                 return QColor('red')
-        """    
+  
         # DEMO 5: Color gradient
         if role == Qt.ItemDataRole.BackgroundRole:
             value = self._data[index.row()][index.column()]
@@ -71,6 +75,37 @@ class TableModel(QAbstractTableModel):
                 value = value + 5
 
                 return QColor(hue[value])
+    """       
+
+    # DEMO 6: Cell decoration
+    def data(self, index:QModelIndex, role):
+
+        if role == Qt.ItemDataRole.DisplayRole:
+            value = self._data[index.row()][index.column()]
+            if isinstance(value, datetime):
+                return value.strftime('%Y-%m-%d')
+            
+            return value
+        
+        elif role == Qt.ItemDataRole.DecorationRole:
+            value = self._data[index.row()][index.column()]
+            if isinstance(value, datetime):
+                return QIcon(path.join(basedir, 'calendar.png'))
+            
+            elif isinstance(value, bool):
+                if value:
+                    return QIcon(path.join(basedir, 'tick.png'))
+                else:
+                    return QIcon(path.join(basedir, 'cross.png'))
+            elif isinstance(value, (int,float)):
+                value = int(value)
+
+                value = max(-5, value)
+                value = min(5, value)
+                value = value + 5
+
+                return QColor(hue[value])
+        
 
     def rowCount(self, index):
         return len(self._data)
@@ -94,12 +129,22 @@ class MainWindow(QMainWindow):
         #     [7.555, 8, 9],
         # ]
 
+        # To use in DEMO 5
+        # data = [
+        #     [4,9,2],
+        #     [1,-1,-1],
+        #     [3,5,-5],
+        #     [3,3,2],
+        #     [7,8,9]
+        # ]
+
+        # To use in DEMO 6
         data = [
-            [4,9,2],
-            [1,-1,-1],
-            [3,5,-5],
-            [3,3,2],
-            [7,8,9]
+            [True, 9, 2],
+            [1, 0, -1],
+            [3, 5, False],
+            [3, 3, 2],
+            [datetime(2019, 5, 4), 8, 9],
         ]
 
         self.model = TableModel(data)
