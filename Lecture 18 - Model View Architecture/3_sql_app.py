6b3405f4252fe1ae37fe79c26fd2e27af6489d80
Andres Anzola <af.anzola@outlook.com>
2025-09-08 23:35:01 -0500

    Showed different ways to change the view (sorting, titles, strategy)

--- Lecture 18 - Model View Architecture/3_sql_app.py
+++ Lecture 18 - Model View Architecture/3_sql_app.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QTableView
+from PySide6.QtWidgets import (QApplication, QMainWindow, QTableView,
+                               QLineEdit, QWidget, QVBoxLayout)
 from PySide6.QtCore import Qt, QSize
 from PySide6.QtSql import QSqlDatabase, QSqlTableModel
 from os import path
@@ -10,7 +11,17 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        container = QWidget()
+        layout = QVBoxLayout()
+
+        self.search = QLineEdit()
+        self.search.textChanged.connect(self.update_filter)
+
         self.table = QTableView()
+
+        layout.addWidget(self.search)
+        layout.addWidget(self.table)
+        container.setLayout(layout)
 
         # In order to connect to a database, a QSqlDatabase instance is to be
         # created. It allows the configuration of the desired SQL
@@ -28,11 +39,54 @@ class MainWindow(QMainWindow):
 
         self.table.setModel(self.model)
 
-        self.model.setTable('Album')
+        # The table, and the database, is editable by default by the user in
+        # the view. To change  the behaviour, Qt allows for strategy config
+
+        """ self.model.setEditStrategy(QSqlTableModel.EditStrategy.OnManualSubmit) """
+
+        self.model.setTable('Track')
+
+        # Sorting columns
+        """ self.model.setSort(2, Qt.SortOrder.DescendingOrder) # Sort column 3 """
+
+        # Sorting columns with field index
+        """ idx = self.model.fieldIndex('Miliseconds') # Column lookup
+        self.model.setSort(idx, Qt.SortOrder.DescendingOrder) """
+
+        # Setting column titles in view
+        """ 
+        self.model.setHeaderData(1, Qt.Orientation.Horizontal, 'Name')
+        self.model.setHeaderData(2, Qt.Orientation.Horizontal, 'Album (ID)')
+        self.model.setHeaderData(3, Qt.Orientation.Horizontal, 'Media Type (ID)')
+        self.model.setHeaderData(4, Qt.Orientation.Horizontal, 'Genre (ID)')
+        self.model.setHeaderData(5, Qt.Orientation.Horizontal, 'Composer')
+        """
+        # Column titles with a dictionary
+        """ 
+        column_titles = {
+            'Name': 'Name',
+            'AlbumId': 'Album (ID)',
+            'MediaTypeId': 'Media Type (ID)',
+            'GenreId': 'Genre (ID)',
+            'Composer': 'Composer'
+        }
+
+        for key, value in column_titles.items():
+            idx = self.model.fieldIndex(key)
+            self.model.setHeaderData(idx, Qt.Orientation.Horizontal, value)
+        """
+
+        # Removing columns (only from view)
+        """ self.model.removeColumns(2,5) """
+
         self.model.select()
 
         self.setMinimumSize(QSize(1024, 600))
-        self.setCentralWidget(self.table)
+        self.setCentralWidget(container)
+
+    def update_filter(self, s):
+        filter_str = f'Name LIKE "%{s}%"' # Uses an SQL WHERE clause
+        self.model.setFilter(filter_str)
 
 
 app = QApplication(sys.argv)
